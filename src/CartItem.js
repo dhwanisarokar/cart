736a7dc91b79a4541b66a35d7affe7b08f27e9d9
dhwanisarokar <dhwanisarokar@gmail.com>
2022-11-02 20:43:38 +0530

    add nore style to CartItem

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,31 @@
 import React from "react";
 
-class CartItem extends React.Component{
-    render(){
+class CartItem extends React.Component {
+    constructor (){
+        super(); // this is used to call parent class constructor
+        this.state = {
+            price: 999,
+            title: "Mobile Phone",
+            qty: 1,
+            img: ''
+        }
+    }
+    render() {
+        const { price, title, qty} = this.state;
         return (
             <div className="cart-item">
                 <div className="left-block" style={styles.image}>
-                    <img alt=""/>
+                    <img alt="" />
                 </div>
                 <div className="right-block">
-                    <div style={{fontSize: 25}}>Phone</div>
-                    <div style={{color: 'grey'}}>Rs 9000 </div>
-                    <div style={{color: 'grey'}}>Qty: 1</div>
+                    <div style={{ fontSize: 25 }}>{title}</div>
+                    <div style={{ color: 'grey' }}>Rs {price} </div>
+                    <div style={{ color: 'grey' }}>Qty: {qty}</div>
                     <div className="cart-item-actions">
                         {/* Buttons */}
+                        <i className="actions-icons fa-solid fa-plus"></i>
+                        <i className="actions-icons fa-solid fa-minus"></i>
+                        <i className="actions-icons fas fa-trash-alt"></i>
                     </div>
                 </div>
             </div>
@@ -22,8 +35,8 @@ class CartItem extends React.Component{
 
 const styles = {
     image: {
-        height: 100,
-        width: 100,
+        height: 110,
+        width: 110,
         borderRadius: 4,
         background: 'gray'
     }
